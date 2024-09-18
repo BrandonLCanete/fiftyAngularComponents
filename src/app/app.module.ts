7dad55033d24fe17c2fa520f72623769cd1ff65b
Brandon L. Canete <brandoncanete35@gmail.com>
2024-09-18 10:06:30 +0800

    #3displayname

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,26 @@ import {AppComponent} from "./app.component";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import { DisplayhelloworldComponent } from './components/displayhelloworld/displayhelloworld.component';
 import { ShowhellobuttonComponent } from './components/showhellobutton/showhellobutton.component';
+import { FormsModule } from '@angular/forms';
+import { DisplaynameComponent } from './components/displayname/displayname.component';
 
 const routes: Routes = [
     {path: ' ', component: DisplayhelloworldComponent},
-    {path: 'showhellobutton', component: ShowhellobuttonComponent}
+    {path: 'showhellobutton', component: ShowhellobuttonComponent},
+    {path: 'displayname', component: DisplaynameComponent}
 ];
-
 @NgModule({
     imports: [
         CommonModule,
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(routes, {enableTracing: true}),
+        FormsModule,
     ], 
     declarations: [
         AppComponent,
         ShowhellobuttonComponent,
+        DisplaynameComponent,
     ],
     providers: [
         provideAnimationsAsync(),
