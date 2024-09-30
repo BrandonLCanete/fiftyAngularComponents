e1f70503fdaa04f5e561539f4ddb6688e78317a3
Brandon L. Canete <brandoncanete35@gmail.com>
2024-09-30 14:45:58 +0800

    #12wordreverser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
 import { TextlengthComponent } from './components/textlength/textlength.component';
 import { CurrencyconverterComponent } from './components/currencyconverter/currencyconverter.component';
 import { EvenoddcheckerComponent } from './components/evenoddchecker/evenoddchecker.component';
+import { WordreverserComponent } from './components/wordreverser/wordreverser.component';
 
 const routes: Routes = [
     {path: ' ', component: DisplayhelloworldComponent},
@@ -30,6 +31,7 @@ const routes: Routes = [
     {path: 'textlength', component: TextlengthComponent},
     {path: 'currencyconverter', component: CurrencyconverterComponent},
     {path: 'evenoddchecker', component: EvenoddcheckerComponent},
+    {path: 'wordreverser', component: WordreverserComponent},
 ];
 @NgModule({
     imports: [
@@ -51,6 +53,7 @@ const routes: Routes = [
         TextlengthComponent,
         CurrencyconverterComponent,
         EvenoddcheckerComponent,
+        WordreverserComponent,
     ],
     providers: [
         provideAnimationsAsync(),
